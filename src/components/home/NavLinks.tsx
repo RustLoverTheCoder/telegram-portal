f94fed00d86bd1fa8efe889ff8257a145b7ad4d5
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-05-31 18:16:38 +0800

    feat: add header

---
+++ src/components/home/NavLinks.tsx
@@ -0,0 +1,40 @@
+/** @jsxImportSource react */
+
+import { useState } from "react";
+import type { FC } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+
+//@ts-ignore
+export const NavLinks: FC = () => {
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  return [
+    ["Features", "#features"],
+    ["Reviews", "#reviews"],
+    ["Pricing", "#pricing"],
+    ["FAQs", "#faqs"],
+  ].map(([label, href], index) => (
+    <a
+      key={label}
+      href={href}
+      className="relative -my-2 -mx-3 rounded-lg px-3 py-2 text-sm text-gray-700 transition-colors delay-150 hover:text-gray-900 hover:delay-[0ms]"
+      onMouseEnter={() => setHoveredIndex(index)}
+      onMouseLeave={() => setHoveredIndex(null)}
+    >
+      <AnimatePresence>
+        {hoveredIndex === index && (
+          <motion.span
+            className="absolute inset-0 rounded-lg bg-gray-100"
+            layoutId="hoverBackground"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1, transition: { duration: 0.15 } }}
+            exit={{
+              opacity: 0,
+              transition: { duration: 0.15, delay: 0.2 },
+            }}
+          />
+        )}
+      </AnimatePresence>
+      <span className="relative z-10">{label}</span>
+    </a>
+  ));
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,25 +1,31 @@
-import { component$ } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+import { QHeader } from "~/components/home/Header";
 
 export default component$(() => {
   return (
     <>
-      <h1>Hi 👋</h1>
-      <p>
-        Can't wait to see what you build with qwik!
-        <br />
-        Happy coding.
-      </p>
+      <QHeader />
+      <main>
+        {/* <QHero /> */}
+        {/* <PrimaryFeatures /> */}
+        {/* <SecondaryFeatures /> */}
+        {/* <CallToAction /> */}
+        {/* <Reviews /> */}
+        {/* <Pricing /> */}
+        {/* <Faqs /> */}
+      </main>
+      {/* <Footer /> */}
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
+  title: "Welcome to Qwik",
   meta: [
     {
-      name: 'description',
-      content: 'Qwik site description',
+      name: "description",
+      content: "Qwik site description",
     },
   ],
 };
