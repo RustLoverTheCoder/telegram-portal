3b0a06f1232d1c78eef664140d72a535a51b5576
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-06-01 10:18:43 +0800

    feat: hero

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,7 +11,7 @@ export default component$(() => {
     <>
       <QHeader />
       <main>
-        <QHero />
+        <QHero client:idle />
         <QPrimaryFeatures />
         <QSecondaryFeatures />
       </main>
