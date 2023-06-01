f407da9240345a361c2171b7f87684b515647b15
MaXiang <71162666+Toursslivers@users.noreply.github.com>
2023-06-01 16:05:42 +0800

    feat: add onRequest

--- src/routes/docs/index.tsx
+++ src/routes/docs/index.tsx
@@ -1,6 +1,15 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import type { RequestHandler } from "@builder.io/qwik-city";
+
+export const onRequest: RequestHandler = ({ headers, query, json }) => {
+  headers.set('Cache-Control', 'private');
+  if (query.get('format') === 'json') {
+    json(200, { message: 'Hello World' });
+  }
+};
+
 export default component$(() => {
   return (
     <>
